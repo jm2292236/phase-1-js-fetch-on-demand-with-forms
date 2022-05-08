92716ce85d1c0dfa09b0c3a292d98702b18df170
Jaime Montes de Oca <jaime0422@live.com>
2022-05-07 20:25:56 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                const title = document.querySelector("section#movieDetails h4");
+                title.innerText = data.title;
+                const summary = document.querySelector("section#movieDetails p");
+                summary.innerText = data.summary;
+            });
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
